ea446c8193606da6d5edf0057630104523664916
M.C. van den Boogaart <scola@kvdb.name>
2016-05-24 14:00:34 +0200

    Use factory methods

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const zlib = require('zlib');
+const Decoder = zlib.Inflate;
+const Encoder = zlib.Deflate;
+const encoding = 'deflate';
+
+function decoder(options) {
+  return {
+    encoding,
+    create: () => new Decoder(options)
+  };
+}
+
+function encoder(options) {
+  return {
+    encoding,
+    create: () => new Encoder(options)
+  };
+}
 
 module.exports = {
-  encoding: 'deflate',
-  Decoder: zlib.Unzip,
-  Encoder: zlib.Gzip
+  encoding,
+  decoder,
+  encoder,
+  Decoder,
+  Encoder
 };
