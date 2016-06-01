9bc22a69083f760a22de9f4a3de7c9e7750262a6
M.C. van den Boogaart <scola@kvdb.name>
2016-06-01 08:33:55 +0200

    Use ES6 modules, with rollup

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const zlib = require('zlib');
-const Decoder = zlib.Inflate;
-const Encoder = zlib.Deflate;
 const encoding = 'deflate';
 
-function decoder(options) {
+import {
+  Inflate as Decoder,
+  Deflate as Encoder
+} from 'zlib';
+
+export const codec = {
+  Decoder,
+  Encoder
+};
+
+export function decoder(options) {
   return {
     encoding,
     create: () => new Decoder(options)
   };
 }
 
-function encoder(options) {
+export function encoder(options) {
   return {
     encoding,
     create: () => new Encoder(options)
   };
 }
-
-module.exports = {
-  encoding,
-  decoder,
-  encoder,
-  Decoder,
-  Encoder
-};
